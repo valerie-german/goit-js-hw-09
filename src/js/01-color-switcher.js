bccabaa58f3014f876cd2c2b0b89cf0f0131ba60
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-06-06 18:04:37 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        const colorCode = getRandomHexColor();
+        body.style.backgroundColor = colorCode;     
+    console.log(`Body color is:  ${colorCode}`);
+    }, 1000);
+    
+    startBtn.setAttribute("disabled", "disabled");
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+    console.log(`Yep, this color is great!`);
+    startBtn.removeAttribute("disabled");
+});
