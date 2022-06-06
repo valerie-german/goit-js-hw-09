57ed4f613c73b6ea47c92acc78ed9a72c3f54bd8
valerie-german <94998541+valerie-german@users.noreply.github.com>
2022-06-06 15:05:16 +0000

    Deploying to gh-pages from @ valerie-german/goit-js-hw-09@bccabaa58f3014f876cd2c2b0b89cf0f0131ba60 🚀

---
+++ 01-color-switcher.3da17aba.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let l=null;t.addEventListener("click",(()=>{l=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t,console.log(`Body color is:  ${t}`)}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(()=>{clearInterval(l),console.log("Yep, this color is great!"),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.3da17aba.js.map
