c588a497aa24b5188cdfc9a9a69f11c463d0129b
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-06-07 20:15:34 +0300

    adds notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const input = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector("button[data-start]");
@@ -14,7 +15,7 @@ let timerId = null;
 
 function dateChecking(today, future) { 
     if (today >= future) {
-        window.alert("The past is irrevocable. Let's look to the future! 🧐");        
+        Notify.failure("The past is irrevocable. Let's look to the future! 🧐");        
     }
     else { 
         startBtn.removeAttribute("disabled");     
@@ -40,7 +41,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function pad(value) { 
+function addLeadingZero(value) { 
     return String(value).padStart(2, '0');
 }
 
@@ -54,12 +55,13 @@ function pad(value) {
                 
                 const { days, hours, minutes, seconds } = convertMs(ms);        
 
-                document.querySelector("span[data-days]").textContent = pad(days);
-                document.querySelector("span[data-hours]").textContent = pad(hours);
-                document.querySelector("span[data-minutes]").textContent = pad(minutes);
-                document.querySelector("span[data-seconds]").textContent = pad(seconds);              
+                document.querySelector("span[data-days]").textContent = addLeadingZero(days);
+                document.querySelector("span[data-hours]").textContent = addLeadingZero(hours);
+                document.querySelector("span[data-minutes]").textContent = addLeadingZero(minutes);
+                document.querySelector("span[data-seconds]").textContent = addLeadingZero(seconds);              
 
             }, 1000); 
+     console.log(timerId);
         } 
 
 const options = {
@@ -77,24 +79,7 @@ const options = {
              timerOn(todaysDate, futureDate);
              console.log(timerId);
          });
-
-        // function timerOn() {
-        //   let ms = futureDate - todaysDate;
-        //     timerId = setInterval(() => {
-        //         ms -= 1000; 
-        //         if (ms <= 0) { 
-        //             clearInterval(timerId);
-        //         }               
-                
-        //         const { days, hours, minutes, seconds } = convertMs(ms);        
-
-        //         document.querySelector("span[data-days]").textContent = pad(days);
-        //         document.querySelector("span[data-hours]").textContent = pad(hours);
-        //         document.querySelector("span[data-minutes]").textContent = pad(minutes);
-        //         document.querySelector("span[data-seconds]").textContent = pad(seconds);              
-
-        //     }, 1000); 
-        // } 
+        
     }    
 };
 
@@ -104,8 +89,3 @@ input.addEventListener('focus', () => {
     flatpickr("#datetime-picker", options);
     
 })
-
-
-    
-
-                
