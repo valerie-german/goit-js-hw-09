c6e60d581fbbaeeab32532123a9541d9ac989d1c
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-06-07 16:18:43 +0300

    json

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,8 +7,6 @@ const startBtn = document.querySelector("button[data-start]");
 startBtn.setAttribute("disabled", "disabled");
 
 const todaysDate = new Date().getTime();
-// console.log(todaysDate);
-// console.log(todaysDate.getTime());
 
 let futureDate;
 let timerId = null;
@@ -16,12 +14,10 @@ let timerId = null;
 
 function dateChecking(today, future) { 
     if (today >= future) {
-        window.alert("The past is irrevocable. Let's look to the future! 🧐");    
-       
+        window.alert("The past is irrevocable. Let's look to the future! 🧐");        
     }
     else { 
-      startBtn.removeAttribute("disabled");
-     
+        startBtn.removeAttribute("disabled");     
     }
 }
 
@@ -44,50 +40,70 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+function pad(value) { 
+    return String(value).padStart(2, '0');
+}
 
-
-
-
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-    onClose(selectedDates) {
-        futureDate = selectedDates[0].getTime();
-         
-        dateChecking(todaysDate, futureDate);
-
-      startBtn.addEventListener('click', timerOn);
-
-
-        function timerOn() {
-          let ms = futureDate - todaysDate;
+ function timerOn(today, future) {
+          let ms = future - today;
             timerId = setInterval(() => {
                 ms -= 1000; 
                 if (ms <= 0) { 
                     clearInterval(timerId);
                 }               
                 
-                const { days, hours, minutes, seconds } = convertMs(ms);
-        
+                const { days, hours, minutes, seconds } = convertMs(ms);        
 
-                document.querySelector("span[data-days]").textContent = days;
-                document.querySelector("span[data-hours]").textContent = hours;
-                document.querySelector("span[data-minutes]").textContent = minutes;
-                document.querySelector("span[data-seconds]").textContent = seconds;
+                document.querySelector("span[data-days]").textContent = pad(days);
+                document.querySelector("span[data-hours]").textContent = pad(hours);
+                document.querySelector("span[data-minutes]").textContent = pad(minutes);
+                document.querySelector("span[data-seconds]").textContent = pad(seconds);              
 
             }, 1000); 
-        }       
+        } 
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  
+    onClose(selectedDates) {
+       futureDate = selectedDates[0].getTime();
+         
+          dateChecking(todaysDate, futureDate);
 
-    },    
+        startBtn.addEventListener('click', () => { 
+             timerOn(todaysDate, futureDate);
+             console.log(timerId);
+         });
+
+        // function timerOn() {
+        //   let ms = futureDate - todaysDate;
+        //     timerId = setInterval(() => {
+        //         ms -= 1000; 
+        //         if (ms <= 0) { 
+        //             clearInterval(timerId);
+        //         }               
+                
+        //         const { days, hours, minutes, seconds } = convertMs(ms);        
+
+        //         document.querySelector("span[data-days]").textContent = pad(days);
+        //         document.querySelector("span[data-hours]").textContent = pad(hours);
+        //         document.querySelector("span[data-minutes]").textContent = pad(minutes);
+        //         document.querySelector("span[data-seconds]").textContent = pad(seconds);              
+
+        //     }, 1000); 
+        // } 
+    }    
 };
 
 
 
-input.addEventListener('focus',  flatpickr("#datetime-picker", options))
+input.addEventListener('focus', () => {
+    flatpickr("#datetime-picker", options);
+    
+})
 
 
     
